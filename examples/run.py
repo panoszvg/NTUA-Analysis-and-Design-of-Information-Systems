42be9c83e3358fdddd558c276f42cecaa4e2fc74
Konstantinos Papaioannou <konpap@arrikto.com>
2022-01-24 00:52:55 +0200

    Fix run.py to wait

--- examples/run.py
+++ examples/run.py
@@ -6,9 +6,14 @@ import time
 queues = json.loads(os.environ.get("QUEUES"))
 queue_ports = json.loads(os.environ.get("QUEUE_PORTS"))
 
+procs = []
+
 for q, p in zip(queues, queue_ports):
     os.environ["QUEUE"] = q
     os.environ["QUEUE_PORT"] = p
     print(f"Open transmitter for {q} in port {p}", flush=True)
-    subprocess.Popen(["python3", "transmitter.py"])
+    proc = subprocess.Popen(["python3", "transmitter.py"])
+    procs.append(proc)
     time.sleep(1)
+
+exit_codes = [p.wait() for p in procs]
