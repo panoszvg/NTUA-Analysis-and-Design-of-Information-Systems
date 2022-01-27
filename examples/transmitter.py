d103a75ad3df5099a11ab121616fd85224e20eb3
Konstantinos Papaioannou <konpap@arrikto.com>
2022-01-27 23:02:11 +0200

    Fix transmitter.py

--- examples/transmitter.py
+++ examples/transmitter.py
@@ -7,46 +7,74 @@ import os
 from pika.exceptions import ChannelClosed, StreamLostError
 
 serverSocket = None
-conn = None
+serverConn = None
 
+brokerConn = None
+brokerChannel = None
+consumerTag = None
+
+# Use callback to send consumed message to the connected client
 def callback(ch, method, properties, body):
+    global serverConn
+
     body = body.decode('utf8').replace("'", '"') + "\n"
     # Send to client
-    global conn
-    conn.sendall(body.encode('utf8'))
+    serverConn.sendall(body.encode('utf8'))
 
-def create_channel(host, port, user, passw):
+
+def start_broker_conn():
+    global brokerConn
+
     # Create connection with message layer
-    creds = pika.PlainCredentials(user, passw)
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=host, port=port, credentials=creds))
-    
-    return connection.channel()
+    creds = pika.PlainCredentials(USERNAME, PASSWORD)
+    brokerConn = pika.BlockingConnection(
+        pika.ConnectionParameters(host=HOSTNAME, port=PORT, credentials=creds))
 
 def start_consume():
-    channel = create_channel(HOSTNAME, PORT, USERNAME, PASSWORD)
-    channel.basic_consume(
-            queue=QUEUE, on_message_callback=callback, auto_ack=True
-        )    
-    channel.start_consuming()
+    global brokerConn
+    global brokerChannel
+    global consumerTag
+
+    brokerChannel = brokerConn.channel()
+    consumerTag = brokerChannel.basic_consume(
+        queue=QUEUE, on_message_callback=callback, auto_ack=True)  
+    brokerChannel.start_consuming()
+
+def stop_consume():
+    global brokerChannel
+    global consumerTag
+    #Close brokerChannel
+    brokerChannel.basic_cancel(consumerTag)
+    brokerChannel.stop_consuming()
+    brokerChannel.close()
 
 def start_stream(): 
-    # Create server that listens for clients
+    # Create server that listens for clients if not already up
     global serverSocket
     if serverSocket == None:
         serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serverSocket.bind((HOSTNAME, QUEUE_PORT))
-    serverSocket.listen(1)
+    serverSocket.listen(0)
+    
+    # Wait for a new client to connect
     print(f'Waiting for client to connect to {QUEUE}.', flush=True)
-    global conn
-    conn, addr = serverSocket.accept()
+    global serverConn
+    serverConn, addr = serverSocket.accept()
     print(f"Client with {addr} connected.", flush=True)
 
-    # Consume queue until an error occurs
     while True:
         try:
+            # Create new connection with broker
+            start_broker_conn()
+
+            # Create new channel and start consuming
             start_consume()
-        except StreamLostError:
+        except StreamLostError as e:
+            print(e, flush=True)
+
+            stop_consume()
+            brokerConn.close()
+
             continue
 
 if __name__ == '__main__':
@@ -58,13 +86,26 @@ if __name__ == '__main__':
     QUEUE = os.environ.get("QUEUE")
     QUEUE_PORT = int(os.environ.get("QUEUE_PORT"))
 
-    conn = None
-
     try:
         start_stream()
-    except (BrokenPipeError, ChannelClosed):
-        print("Exception ChannelClosed: Restarting channel", flush=True)
-        conn.close()
+    except (BrokenPipeError, ChannelClosed, socket.error) as e:
+        print(e, flush=True)
+        print("Restarting process", flush=True)
+
+        # Close connected client
+        serverConn.close()
+
+        #Close brokerChannel
+        stop_consume()
+
+        #Close brokerConn
+        brokerConn.close()
+
+        # Restart server
         start_stream()
+
     except KeyboardInterrupt:
-        conn.close()
+        serverConn.close()
+        serverSocket.close()
+        stop_consume()
+        brokerConn.close()
